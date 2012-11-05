f82bc35e2a23af4e0e6e64e509148fb5c2f8c972
Danny Cook <Danny.Cook@gatech.edu>
2012-11-05 18:49:57 -0500

    Hooked custom view classes to pre-existing view controllers, for easy programming and modification.

---
+++ Caravan/CurrentViewController.m
@@ -0,0 +1,38 @@
+//
+//  CurrentViewController.m
+//  Caravan
+//
+//  Created by Danny Cook on 11/5/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import "CurrentViewController.h"
+
+@interface CurrentViewController ()
+
+@end
+
+@implementation CurrentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Caravan/JoinViewController.m
@@ -0,0 +1,38 @@
+//
+//  JoinViewController.m
+//  Caravan
+//
+//  Created by Danny Cook on 11/5/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import "JoinViewController.h"
+
+@interface JoinViewController ()
+
+@end
+
+@implementation JoinViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
