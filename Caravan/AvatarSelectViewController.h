df89a5803067c724cbf3a291051eb23a9c0981cb
Danny Cook <Danny.Cook@gatech.edu>
2012-11-05 19:05:16 -0500

    Added and hooked up custom classes for all profile editing functions.

---
+++ Caravan/AvatarSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AvatarSelectViewController.h
+//  Caravan
+//
+//  Created by Danny Cook on 11/5/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AvatarSelectViewController : UICollectionViewController
+
+@end
---
+++ Caravan/PreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesViewController.h
+//  Caravan
+//
+//  Created by Danny Cook on 11/5/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreferencesViewController : UIViewController
+
+@end
