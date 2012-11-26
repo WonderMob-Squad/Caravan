453079575828a50f9aae106a76f45c21682da143
Danny Cook <Danny.Cook@gatech.edu>
2012-11-25 20:15:24 -0500

    Updated MapView to enable user location and zooming functionality. Next up on the list is the addition of database calls and annotations!

---
+++ Caravan/MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  Caravan
+//
+//  Created by Danny Cook on 11/25/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ Caravan/MapViewController.m
@@ -0,0 +1,58 @@
+//
+//  MapViewController.m
+//  Caravan
+//
+//  Created by Danny Cook on 11/25/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+
+@implementation MapViewController
+@synthesize mapView = _mapView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    _mapView.delegate = (id)self;
+    NSLog(@"Delegate declared");
+}
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    MKCoordinateRegion mapRegion;
+    mapRegion.center = mapView.userLocation.coordinate;
+    mapRegion.span.latitudeDelta = 0.2;
+    mapRegion.span.longitudeDelta = 0.2;
+    
+    [mapView setRegion:mapRegion animated: YES];
+    NSLog(@"User location updated. Region set.");
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [self setMapView:nil];
+    [super viewDidUnload];
+}
+@end
