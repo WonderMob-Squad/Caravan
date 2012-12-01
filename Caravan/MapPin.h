5e353333d794ec4fffa0e11e7977fe4619e4b95b
Danny Cook <Danny.Cook@gatech.edu>
2012-12-01 17:15:49 -0500

    Added MapPin files.

---
+++ Caravan/MapPin.h
@@ -0,0 +1,13 @@
+//
+//  MapPin.h
+//  Caravan
+//
+//  Created by Danny Cook on 12/1/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MapPin : MKAnnotationView
+
+@end
---
+++ Caravan/MapPin.m
@@ -0,0 +1,31 @@
+//
+//  MapPin.m
+//  Caravan
+//
+//  Created by Danny Cook on 12/1/12.
+//  Copyright (c) 2012 Danny Cook. All rights reserved.
+//
+
+#import "MapPin.h"
+
+@implementation MapPin
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
